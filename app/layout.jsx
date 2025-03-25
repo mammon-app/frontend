6689e3a709cd4230cba9430d3428b51b440b4b6c
devcharlezen <devcharlezen@gmail.com>
2025-03-25 20:16:25 +0100

    chore: Fixed responsiveness in dashboard and topnav

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,13 +26,7 @@ export const metadata = {
         alt: "Mammon App Logo",
       },
     ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Mammon App",
-    description: "With Mammon App, you can turn your crypto into cash",
-    images: ["https://www.mammonapp.com/images/mammon-app-favicon.svg"],
-  },
+  }
 };
 
 
@@ -40,17 +34,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <meta property="og:title" content="Mammon App" />
-        <meta property="og:description" content="With Mammon App, you can turn your crypto into cash" />
-        <meta property="og:image" content="https://www.mammonapp.com/images/mammon-app-favicon.svg" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.mammonapp.com" />
-        
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Mammon App" />
-        <meta name="twitter:description" content="With Mammon App, you can turn your crypto into cash" />
-        <meta name="twitter:image" content="https://www.mammonapp.com/images/mammon-app-favicon.svg" />
-
       <link rel="icon" href="./images/mammon-app-favicon.svg" sizes="any" />
       <body className={helveticaNeue.className}>{children}</body>
     </html>
